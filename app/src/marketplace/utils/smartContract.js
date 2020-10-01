8efe9b8bf1459900e74840bc56be5b06dc966b18
Juha Haavisto <juha.haavisto@sujuwa.fi>
2020-10-01 16:27:17 +0300

    Get DU information from streamr-client

--- app/src/marketplace/utils/smartContract.js
+++ app/src/marketplace/utils/smartContract.js
@@ -17,14 +17,11 @@ import type {
     Address,
     SmartContractConfig,
     SmartContractTransaction,
-    SmartContractDeployTransaction,
-    SmartContractMetadata,
 } from '$shared/flowtype/web3-types'
 import type { Product, SmartContractProduct } from '../flowtype/product-types'
 import type { NumberString } from '$shared/flowtype/common-types'
 
 import Transaction from '$shared/utils/Transaction'
-import DeployTransaction from '$shared/utils/DeployTransaction'
 import { gasLimits } from '$shared/utils/constants'
 
 export type Callable = {
@@ -116,52 +113,6 @@ export const send = (method: Sendable, options?: {
                     }
                 })
                 .catch(errorHandler)
-        }, errorHandler)
-
-    return tx
-}
-
-export const deploy = (contract: SmartContractMetadata, args: Array<any>, options?: {
-    gas?: number,
-}): SmartContractDeployTransaction => {
-    const web3 = getWeb3()
-    const emitter = new EventEmitter()
-    const errorHandler = (error: Error) => {
-        emitter.emit('error', error)
-    }
-    const tx = new DeployTransaction(emitter)
-    Promise.all([
-        web3.getDefaultAccount(),
-        checkEthereumNetworkIsCorrect(web3),
-    ])
-        .then(([account]) => Promise.all([
-            Promise.resolve(account),
-            // Calculate future address of the contract so that we don't have to wait
-            // for the transaction to be confirmed.
-            calculateContractAddress(account),
-        ]))
-        .then(([account, futureAddress]) => {
-            const web3Contract = new web3.eth.Contract(contract.abi)
-            const deployer = web3Contract.deploy({
-                data: contract.bytecode,
-                arguments: args,
-            })
-            deployer
-                .send({
-                    gas: (options && options.gas) || gasLimits.DEPLOY_DATA_UNION,
-                    from: account,
-                })
-                .on('error', errorHandler)
-                .on('transactionHash', (hash) => {
-                    emitter.emit('transactionHash', hash, futureAddress)
-                })
-                .on('receipt', (receipt) => {
-                    if (parseInt(receipt.status, 16) === 0) {
-                        errorHandler(new TransactionError(I18n.t('error.txFailed'), receipt))
-                    } else {
-                        emitter.emit('receipt', receipt)
-                    }
-                })
         }, errorHandler)
 
     return tx
